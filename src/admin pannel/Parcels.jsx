dcadde41712ea12eabe2b4b3718ab24ffcba0af5
Meet-Prajapati-MP <pmeet9174@gmail.com>
2026-07-13 22:32:04 +0530

    building parcel

--- src/admin pannel/Parcels.jsx
+++ src/admin pannel/Parcels.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import './Parcels.css'
+import { FaRupeeSign , FaUserCircle } from "react-icons/fa";
+import { FiBox, FiTruck } from "react-icons/fi";
+import { IoTimeOutline } from "react-icons/io5"
 import Anavbar from "./Anavbar";
 
 
@@ -8,19 +11,51 @@ const Parcels = () =>{
         <>
         <Anavbar/>
         <header>
-        <div className="page-header">
-           <div className="page-name">
-                <h2>Parcels</h2>
-            </div>
-                <button className="page-btn">View Website</button>
-        </div>  
-        </header>
+                <div className="page-header">
+                   <div className="parcel-input">
+                        <input type="text" placeholder="Search LR.NO., Customer or, Route..." className="search-input" />
+                        <div className="new-btn">
+                          <button className="new-shipment">+ New Shipment</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                          <div className="userdetails">
+                             <h3>Admin User</h3>
+                             <h5>OPERATION LEAD</h5>
+                          </div>
+                          <div className="userimg">
+                             <FaUserCircle/>
+                          </div>
+                    </div>
+                </div>  
+                </header>
         <div className="parcels">
             <div className="parcels-container">
-               <div className="input-container">
-                 <input type="text" placeholder="Search parcels..." className="search-input"/>
-                 <button className="input-btn">+ Add Parcel</button>
-                </div> 
+                <div className="parcel-top">
+                    <div className="top-txt">
+                        <h1>Parcels Management</h1>
+                        <p>Manage,track, and analyze all active shipments across routes.</p>
+                    </div>
+                    <div className="tot-active">
+                       <div className="active-img">
+                            <FiTruck/>
+                        </div>
+                        <div className="text">
+                            <h4>Total Active</h4>
+                            <p>0</p>
+                        </div>   
+                    </div>
+                    <div className="pending">
+                       <div className="pending-img">
+                            <IoTimeOutline/>
+                        </div>
+                        <div className="text">
+                            <h4>Pending</h4>
+                            <p>0</p>
+                        </div>   
+                    </div>
+                </div>
+                
                 <div className="table-container">
                     <div className="table-row">
                         <table className="table">
@@ -40,7 +75,7 @@ const Parcels = () =>{
                     </div>
                 </div>
             </div>
-        </div>
+         </div>
         </>
     );
 }
